fffd65b3a8159647638bbb2fa17abf0db422508d
cristiansep <cristiansepulvedamendez@gmail.com>
2020-02-02 16:53:40 -0300

    Sistema de rutas listo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import {Layout} from 'antd';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-
-
+//Pages
+import Home from './pages/Home';
+import NewMovie from './pages/New-movie';
+import Movie from './pages/Movie';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
+import Error404 from './pages/Error404';
 
 
 function App() {
+
+  const {Header, Content} = Layout;
+
   return (
-    <div>
-      <h1>Estamos en App</h1>
-   
-    </div>
+    <Layout>
+     <Router>
+     <Header>
+         Header...
+       </Header>
+       <Content>
+         <Switch>
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/new-movie" component={NewMovie}/>
+         <Route exact path="/popular" component={Popular}/>
+         <Route exact path="/search" component={Search}/>
+         <Route exact path="/movie/:id" component={Movie}/>
+         <Route exact path="*" component={Error404}/>
+       </Switch>
+       </Content>
+       
+       
+     </Router>
+    </Layout>
   );
 }
 
