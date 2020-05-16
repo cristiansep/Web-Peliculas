1a479666ae9daf6a83b8597a6647c82ac20680df
cristiansep <cristiansepulvedamendez@gmail.com>
2020-05-15 23:12:27 -0400

    Page ultimos lanzamientos lista

--- src/pages/New-movie.js
+++ src/pages/New-movie.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import {Row, Col} from 'antd';
+import {URL_API, API} from '../config/config';
+import Footer from '../components/Footer';
+import Loading from '../components/Loading';
+import MovieCatalog from '../components/MovieCatalog/MovieCatalog';
 
 const NewMovie = () => {
+    const [movieList, setMovieList] = useState([]);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                `${URL_API}/movie/now_playing?api_key=${API}&lenguage=es-ES&page=${page}`
+                );
+                const movies = await response.json();
+                setMovieList(movies);
+        })();
+    }, [page]);
+
     return (
-       'Estamos en new movie'
+        <Row>
+            <Col span="24" style={{textAlign: "center", marginTop: 25}}>
+                <h1 style={{fontSize: 35, fontWeight: "bold"}}>
+                    Ultimos lanzamientos
+                    </h1>
+            </Col>
+            {movieList.results ? (
+                <Col span="24">
+                <MovieCatalog movies={movieList}/>
+            </Col>
+            ) : (
+                <Col span="24">
+                  <Loading/>  
+                </Col>
+            )}
+            
+            <Col span="24">
+                <Footer/>
+            </Col>
+           
+        </Row>
     );
-};
+
+}
 
 export default NewMovie;
